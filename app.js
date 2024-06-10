9ba5e034331481dbb0fde45deb6e7cf13f143b1b
Tomas Vergara <tominacho@hotmail.com>
2024-06-10 12:55:28 -0300

    actualizados datos en db

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const db = require("./config/sequelize-init");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const newsletterRouter = require("./routes/newsletter.routes");
 app.use("/api", newsletterRouter);
